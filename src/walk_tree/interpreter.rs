3d759f8cc175b8d0af97a175723623a865819f02
Piotr Ackermann <pioacker@gmail.com>
2023-01-04 09:23:48 +0100

    Interpreter

--- src/walk_tree/interpreter.rs
+++ src/walk_tree/interpreter.rs
@@ -5,21 +5,23 @@ use super::{
     value::{self, Value},
 };
 
-pub fn interpret(expr: &Expr) -> Value {
+pub fn interpret(expr: &Expr) -> RValue {}
+
+fn interpret_expr(expr: &Expr) -> Value {
     match expr {
         Expr::Literal(literal) => interpret_literal(literal),
-        Expr::Grouping(expr) => interpret(expr),
+        Expr::Grouping(expr) => interpret_expr(expr),
         Expr::Unary(operator, operand) => interpret_unary(operator, operand),
         Expr::Binary(left, operator, right) => interpret_binary(left, operator, right),
     }
 }
 
-pub fn interpret_literal(literal: &Value) -> Value {
+fn interpret_literal(literal: &Value) -> Value {
     literal.to_owned()
 }
 
-pub fn interpret_unary(operator: &Token, right: &Expr) -> Value {
-    let right = interpret(right);
+fn interpret_unary(operator: &Token, right: &Expr) -> Value {
+    let right = interpret_expr(right);
     match operator.kind {
         TokenKind::Minus => value::unary_operation::<f64>(|a| -a, right),
         TokenKind::Bang => Value::from(!right.is_truthy()),
@@ -27,9 +29,9 @@ pub fn interpret_unary(operator: &Token, right: &Expr) -> Value {
     }
 }
 
-pub fn interpret_binary(left: &Expr, operator: &Token, right: &Expr) -> Value {
-    let left = interpret(left);
-    let right = interpret(right);
+fn interpret_binary(left: &Expr, operator: &Token, right: &Expr) -> Value {
+    let left = interpret_expr(left);
+    let right = interpret_expr(right);
     match operator.kind {
         TokenKind::Minus => value::binary_operation::<f64>(|a, b| a + b, left, right),
         TokenKind::Plus => {
@@ -47,8 +49,8 @@ pub fn interpret_binary(left: &Expr, operator: &Token, right: &Expr) -> Value {
         TokenKind::GreaterEqual => value::binary_relation::<f64>(|a, b| a >= b, left, right),
         TokenKind::Less => value::binary_relation::<f64>(|a, b| a < b, left, right),
         TokenKind::LessEqual => value::binary_relation::<f64>(|a, b| a <= b, left, right),
-        TokenKind::BangEqual => value::binary_relation::<f64>(|a, b| a != b, left, right),
-        TokenKind::EqualEqual => value::binary_relation::<f64>(|a, b| a == b, left, right),
+        TokenKind::BangEqual => Value::from(left != right),
+        TokenKind::EqualEqual => Value::from(left == right),
         _ => unreachable!(),
     }
 }
